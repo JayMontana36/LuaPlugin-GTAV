69868f98902bbe782d28dfeac33185b26b0c4328
Jasen Samuels <jasensamuels@gmail.com>
2023-01-25 08:48:01 -0500

    Update main.lua
    Version

--- GameDir-GTAV/scripts/main.lua
+++ GameDir-GTAV/scripts/main.lua
@@ -6,7 +6,7 @@ Scripts_Path    = "scripts\\ScriptsDir-Lua\\" or "C:\\Path\\To\\ScriptsDir-Lua\\
 
 -- Script/Code Area
 --[[ Define JM36 LP Version ]]
-JM36_GTAV_LuaPlugin_Version=20221218.0
+JM36_GTAV_LuaPlugin_Version=20230125.0
 
 
 
