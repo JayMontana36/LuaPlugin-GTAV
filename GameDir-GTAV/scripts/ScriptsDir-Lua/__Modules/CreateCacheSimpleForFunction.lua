ee7da2cacf0af70ee7c1e1b43218b81f5509a728
Jasen Samuels <jasensamuels@gmail.com>
2023-04-14 03:00:18 -0400

    Update CreateCacheSimpleForFunction.lua

--- GameDir-GTAV/scripts/ScriptsDir-Lua/__Modules/CreateCacheSimpleForFunction.lua
+++ GameDir-GTAV/scripts/ScriptsDir-Lua/__Modules/CreateCacheSimpleForFunction.lua
@@ -1,9 +1,10 @@
 local setmetatable = setmetatable
 local __call = function(Self,Key)return Self[Key]end
+local __cache = {}
 local metatable =
 {
 	__mode	=	"kv",
-	__index =	function(Self,Key)local Value=Self.__SrcFunc(Key)Self[Key]=Value;return Value;end,
+	__index =	function(Self,Key)local Value=__cache[Self](Key);Self[Key]=Value;return Value;end,
 	__call	=	__call,
 }
 return setmetatable
@@ -11,7 +12,7 @@ return setmetatable
 	{},
 	{
 		__mode="v",
-		__index=function(Self,SrcFunc)local Value=setmetatable({__SrcFunc=SrcFunc},metatable)Self[SrcFunc]=Value;return Value;end,
-		__call,
+		__index=function(Self,SrcFunc)local Value=setmetatable({},metatable);Self[SrcFunc]=Value;__cache[Value]=SrcFunc;return Value;end,
+		__call=__call,
 	}
 )
