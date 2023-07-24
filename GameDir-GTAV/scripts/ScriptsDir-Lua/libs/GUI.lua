3b5aa3565538ed04cfba829542f2fe110ab9f180
Jasen Samuels <jasensamuels@gmail.com>
2023-07-24 07:17:25 -0400

    Update GUI.lua
    Basic updates and optimizations, very little modernizations.

--- GameDir-GTAV/scripts/ScriptsDir-Lua/libs/GUI.lua
+++ GameDir-GTAV/scripts/ScriptsDir-Lua/libs/GUI.lua
@@ -1,3 +1,24 @@
+local Info = Info
+
+local print = print
+local type = type
+local ipairs = ipairs
+local pairs = function(t)return next,t end -- avoid using __pairs in Lua 5.2+
+
+local DrawRect = DrawRect
+local GetScreenResolution = GetScreenResolution
+local SetTextFont = SetTextFont
+local SetTextScale = SetTextScale
+local SetTextColour = SetTextColour
+local SetTextCentre = SetTextCentre
+local SetTextDropshadow = SetTextDropshadow
+local SetTextEdge = SetTextEdge
+local BeginTextCommandDisplayText = BeginTextCommandDisplayText
+local AddTextComponentSubstringPlayerName = AddTextComponentSubstringPlayerName
+local EndTextCommandDisplayText = EndTextCommandDisplayText
+
+local get_key_pressed = get_key_pressed
+
 local GUI = {}
 GUI.GUI = {}
 GUI.buttonCount = 0
@@ -6,7 +27,7 @@ GUI.selection = 0
 GUI.time = 0
 GUI.hidden = false
 function GUI.addButton(name, func,args, xmin, xmax, ymin, ymax)
-	print("Added Button"..name )
+	print("Added Button "..name )
 	GUI.GUI[GUI.buttonCount +1] = {}
 	GUI.GUI[GUI.buttonCount +1]["name"] = name
 	GUI.GUI[GUI.buttonCount+1]["func"] = func
@@ -18,18 +39,15 @@ function GUI.addButton(name, func,args, xmin, xmax, ymin, ymax)
 	GUI.GUI[GUI.buttonCount+1]["ymax"] = ymax 
 	GUI.buttonCount = GUI.buttonCount+1
 end
-function GUI.unload()
-end
 function GUI.init()
-
 	GUI.loaded = true
 end
 function GUI.tick()
 	if(not GUI.hidden)then
 		if( GUI.time == 0) then
-			GUI.time = GAMEPLAY.GET_GAME_TIMER()
+			GUI.time = Info.Time
 		end
-		if((GAMEPLAY.GET_GAME_TIMER() - GUI.time)> 100) then
+		if((Info.Time - GUI.time)> 100) then
 			GUI.updateSelection()
 		end	
 		GUI.renderGUI()	
@@ -72,30 +90,27 @@ function GUI.renderGUI()
 	 GUI.renderButtons()
 end
 function GUI.renderBox(xMin,xMax,yMin,yMax,color1,color2,color3,color4)
-	GRAPHICS.DRAW_RECT(xMin, yMin,xMax, yMax, color1, color2, color3, color4);
+	DrawRect(xMin, yMin,xMax, yMax, color1, color2, color3, color4)
 end
 
 function GUI.renderButtons()
-	
 	for id, settings in pairs(GUI.GUI) do
-		local screen_w = 0
-		local screen_h = 0
-		screen_w, screen_h =  GRAPHICS.GET_SCREEN_RESOLUTION(0, 0)
-		boxColor = {70,95,95,255}
+		local screen_w, screen_h = GetScreenResolution(0, 0)
+		local r, g, b, a = 70, 95, 95, 255
 		if(settings["active"]) then
-			boxColor = {218,242,216,255}
+			r, g, b, a = 218, 242, 216, 255
 		end
-		UI.SET_TEXT_FONT(0)
-		UI.SET_TEXT_SCALE(0.0, 0.35)
-		UI.SET_TEXT_COLOUR(255, 255, 255, 255)
-		UI.SET_TEXT_CENTRE(true)
-		UI.SET_TEXT_DROPSHADOW(0, 0, 0, 0, 0)
-		UI.SET_TEXT_EDGE(0, 0, 0, 0, 0)
-		UI._SET_TEXT_ENTRY("STRING")
-		UI._ADD_TEXT_COMPONENT_STRING(settings["name"])
-		UI._DRAW_TEXT(settings["xmin"]+ 0.05, (settings["ymin"] - 0.0125 ))
-		UI._ADD_TEXT_COMPONENT_STRING(settings["name"])
-		GUI.renderBox(settings["xmin"] ,settings["xmax"], settings["ymin"], settings["ymax"],boxColor[1],boxColor[2],boxColor[3],boxColor[4])
+		SetTextFont(0)
+		SetTextScale(0.0, 0.35)
+		SetTextColour(255, 255, 255, 255)
+		SetTextCentre(true)
+		SetTextDropshadow(0, 0, 0, 0, 0)
+		SetTextEdge(0, 0, 0, 0, 0)
+		BeginTextCommandDisplayText("STRING")
+		AddTextComponentSubstringPlayerName(settings["name"])
+		EndTextCommandDisplayText(settings["xmin"]+ 0.05, (settings["ymin"] - 0.0125 ))
+		--AddTextComponentSubstringPlayerName(settings["name"])
+		GUI.renderBox(settings["xmin"] ,settings["xmax"], settings["ymin"], settings["ymax"], r, g, b, a)
 	 end     
 end
 return GUI
